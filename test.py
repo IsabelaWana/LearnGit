ab5e59cb5bda81301ed71e1ad77ce2c33d8193ba
isabela <isabela.puscasu@gmail.com>
2020-05-01 21:46:16 +0300

    Second Commit

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 def func(name, subject): 
-    print("My name is " + name + " and I am learning " + subject + ".")
+    print("My name is " + name + " and I am learning " + subject + ". </br> I just pushed my first commit :)")
 
 func('Isabela', 'Git')
